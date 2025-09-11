e65bed95a1b4d81b09a4216dac587bdc2ba83878
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-11 22:16:41 +0530

    search component completed

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import { FaSearch } from 'react-icons/fa'
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./Search.css";
 
-const Search = ({type,value,handleChange}) => {
+const Search = ({ type, value, handleChange }) => {
+  const[keyChecker, setKeyChecker] = useState(false);
   return (
-    <div>     
-        <FaSearch/>
-        <input 
-            type = {type}
-            value = {value}
-            onChange = {handleChange}
-            placeholder= 'Search here...'
-        
-        />
-           
+    <div className="search-container">
+      <div className="search-bar">
+        <div className="search-icon">
+          <FaSearch />
+        </div>
+        <div className="search-input">
+          <input
+            type={type}
+            value={value}
+            onChange={handleChange}
+            onKeyDown={(e) => {
+              setKeyChecker(e.key === "Enter") //direct implementation
+              // if (e.key === "Enter"){
+              //   setKeyChecker(true)
+              // } else {
+              //   setKeyChecker(false)
+              // }              
+            }}
+           // style={(value === "" && keyChecker) ? {border:"1px solid red"}:{border:"1px solid purple"}} //ternary operator(if else condition)
+            style={{border:`1px solid ${(value === "" && keyChecker) ? "red":"purple"}`}} //string interpolation
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
