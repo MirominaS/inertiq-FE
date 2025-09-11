9131836722a050a32057906459055c2d21e24fcc
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-11 22:16:20 +0530

    button component completed

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Button.css'
 
 const Button = ({label,onClick}) => {
   return (
-    <>
+    <div className='button-container'>
     <button onClick = {onClick}> {label}</button>        
-    </>
-  )
+    </div>
+  ) 
 }
 
 export default Button
