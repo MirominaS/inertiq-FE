a4cbdfec8300ef58ba30d070af202d8a84059c79
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-13 16:44:13 +0530

    styles added to logo component

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Logo.css'
 
 const Logo = ({height="auto"}) => {
   return (
-    <>
+    <div className='logo-container'>
         <img src="/src/assets/main/logo.svg" alt="logo" height={height} />
 
-    </>
+    </div>
   )
 }
 
