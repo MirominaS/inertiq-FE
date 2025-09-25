3f8c5b2300016bae29d2bdad7e4e0af47a759bb7
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-25 10:53:30 +0530

    initiated Newsletter component

---
+++ src/components/newsLetter/NewsLetter.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './NewsLetter.css'
+const NewsLetter = () => {
+  const handleClick = () => {
+      alert("Button Clicked!")
+  }
+  return (
+    <div className='newsletter-container'>
+        <div className='newsletter-details'>
+            <div className='newsletter-text'>
+                <div className='heading'>Stay Tuned!</div>
+                <div className='text'>Subscribe to receive the latest news and updates about TDA.
+                        We promise not to spam you!
+                 </div>
+               
+            </div>
+            <div className='newsletter-input'>
+                <input type="text" placeholder='Enter email address' />
+                <button onClick={handleClick}>continue</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default NewsLetter
