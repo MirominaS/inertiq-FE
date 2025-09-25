f99d680b8cbf42c5d254aee2dc57aed9c5bab601
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-25 17:27:32 +0530

    added components to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import Navbar from '../../components/navbar/Navbar'
+import Card from '../../components/card/Card'
+import TopCarousel from '../../components/topCarousel/TopCarousel'
+import image1 from '/src/assets/main/students.jpg'
+import StateCount from '../../components/stateCount/StateCount'
+import FooterMain from '../../components/footerMain/FooterMain'
+import FooterBasic from '../../components/footerBasic/FooterBasic'
+import NewsLetter from '../../components/newsLetter/NewsLetter'
+import HomeBanner from '../../components/homeBanner/HomeBanner'
+import './Home.css'
 const Home = () => {
+  const studentsDetails = [
+    {id:0,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
+    {id:1,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
+    {id:2,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
+    {id:3,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
+    {id:4,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"}
+  ]
   return (
-    <div>Homeeeeeeee</div>
+    <div className='home-container'>
+       <Navbar/>
+       <HomeBanner/>
+       <TopCarousel cardDetails={studentsDetails}/>       
+       <NewsLetter/>
+       <FooterMain/>
+       <FooterBasic/>
+    </div>
   )
 }
 
