9c05b323adb2f0a14b7b27236430ccfa6e55c0ff
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-25 17:25:49 +0530

    removed navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import './App.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
+
 
 function App() {
  
   return (
     <div className='app'>
-      <Navbar/>
       <BrowserRouter>
           <Routes>
             <Route path = "/" element={<Home/>}/>
