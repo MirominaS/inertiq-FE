3e5799c7faf3fe6f6b3afb61f32d82c68ec0ba3b
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-25 10:51:04 +0530

    Basic footer component initiated

---
+++ src/components/footerBasic/FooterBasic.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './FooterBasic.css'
+
+const FooterBasic = () => {
+  return (
+    <div className='footer-basic-container'>
+        <div className='footer-basic-left'>
+            <div>logo</div>
+            <div>copyrights</div>
+        </div>
+        <div className='footer-basic-right'>
+            <div>fb</div>
+            <div>linkedin</div>
+            <div>insta</div>
+            <div>x</div>
+        </div>
+    </div>
+  )
+}
+
+export default FooterBasic
