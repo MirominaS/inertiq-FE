e9a448c9d58281a684fbd5672e4117d40ada7a6e
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-25 17:21:56 +0530

    updated footer basci component with icons and logo

--- src/components/footerBasic/FooterBasic.jsx
+++ src/components/footerBasic/FooterBasic.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import './FooterBasic.css'
+import Logo from '../logo/Logo'
+import { FaRegCopyright } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
 
 const FooterBasic = () => {
   return (
     <div className='footer-basic-container'>
         <div className='footer-basic-left'>
-            <div>logo</div>
-            <div>copyrights</div>
+            <div>
+              <Logo height='60px'/>
+            </div>
+            <div className='footer-copy-rights'>
+              <FaRegCopyright />
+              <p>2025 All Rights Reserved</p>
+            </div>
         </div>
         <div className='footer-basic-right'>
-            <div>fb</div>
-            <div>linkedin</div>
-            <div>insta</div>
-            <div>x</div>
+            <div><FaFacebookF /></div>
+            <div><FaLinkedin /></div>
+            <div><FaInstagram /></div>
+            <div><FaXTwitter /></div>
         </div>
     </div>
   )
