bd72a30b0d3ba6f1200b4af96fe2c1254cebf002
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-27 11:55:59 +0530

    image changed to top carousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/navbar/Navbar'
 import Card from '../../components/card/Card'
 import TopCarousel from '../../components/topCarousel/TopCarousel'
 import image1 from '/src/assets/main/students.jpg'
+import image2 from '/src/assets/main/homeScreen.svg'
 import StateCount from '../../components/stateCount/StateCount'
 import FooterMain from '../../components/footerMain/FooterMain'
 import FooterBasic from '../../components/footerBasic/FooterBasic'
@@ -12,7 +13,7 @@ import './Home.css'
 const Home = () => {
   const studentsDetails = [
     {id:0,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
-    {id:1,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
+    {id:1,image: image2 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
     {id:2,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
     {id:3,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"},
     {id:4,image: image1 ,name:"Sanvy",stream:"Bio Science",percentage:"68%"}
