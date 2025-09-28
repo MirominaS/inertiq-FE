7a5f4eb04d176e9752f448030bb6636b306260ff
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-28 21:00:38 +0530

    added type to the button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Button.css'
 
-const Button = ({label,onClick,width='100px'}) => {
+const Button = ({label,onClick,width='100px',type}) => {
   return (
     <div className='button-container'>
-    <button onClick = {onClick} style={{width}}> {label}</button>        
+    <button onClick = {onClick} style={{width}} type={type}> {label}</button>        
     </div>
   ) 
 }
