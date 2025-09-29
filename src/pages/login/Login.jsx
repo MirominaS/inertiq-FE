5b963fa99dd9bf20be6da30fb481efe7412c0adc
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-09-29 10:20:03 +0530

    added login validation and login with google button

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -6,23 +6,72 @@ import FooterBasic from '../../components/footerBasic/FooterBasic'
 import Button from '../../components/button/Button'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
-  const [userNameChange, setUserNameChange] = useState('');
-  const [passwordChange,setPasswordChange] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password,setPassword] = useState('');
+  const [showPassword,setShowPassword] = useState(false)
+  const [error,setError] = useState({})
+  const [displayMsg,setDisplayMsg] = useState("")
 
-  const handleUserNameChange = (uname) => {
-        setUserNameChange(uname.target.value)
+  const handleUserName = (uname) => {
+        setUserName(uname.target.value)
     }
 
-    const handlePasswordChange = (pswd) => {
-      setPasswordChange(pswd.target.event)
+    const handlePassword = (pswd) => {
+      setPassword(pswd.target.value)
     }
 
+    const handlePasswordIcon = () => {
+      setShowPassword(!showPassword)
+    }
+
+    const validation = () => {
+      let error = {username:"",pswrd:""};
+      let valid = true;
+      if(!userName.trim()){
+        error.username = "Username is required"
+        setUserName("")
+        valid = false;
+      }else if(userName.length < 5){
+        error.username = "Username should be atlest 6 charectors"
+        setUserName("")
+        valid = false;
+      }
+      if(!password.trim()){
+        error.pswrd = "Password is required"
+        setPassword("")
+        valid = false;
+      }else if(password.length < 8){
+        error.pswrd = "Password should contain atleast 8 char"
+        setPassword("")
+        valid = false;
+      }
+      setError(error)
+      return valid;
+    }    
+
+    const handleClick = () => {
+      const validate = validation()
+      let message = ""
+      if(validate ===true){
+        message = "Login Success!"
+        setUserName("")
+        setPassword("")
+      }else if (userName.length !== 0 || password.length !== 0){
+        validationUserName()
+        validationPassword() 
+      }else{
+        message = "Enter Username and Password"
+      }
+      setDisplayMsg(message)
+    }
   return (
-    <div className='login-container' >
+    <div className='login-container'>
       <div className='login-logo'>
         <Logo height='80px'/>
+        <span className='login-message'>{displayMsg}</span>
       </div>
 
       <div className='login-input'>
@@ -32,8 +81,10 @@ const Login = () => {
             height='35px'
             width='400px' 
             placeholder='Username' 
-            value={userNameChange}
-            handleChange={handleUserNameChange}/>
+            value={userName}
+            handleChange={handleUserName}
+          />
+            {error.username && <span className='login-message'>{error.username}</span>}
         </div>
         <div className='login-password'>
           <div className='login-password-input'>
@@ -41,19 +92,20 @@ const Login = () => {
               height='35px' 
               width='400px' 
               placeholder='Password'
-              value={passwordChange}
-              handleChange={handlePasswordChange}
+              type={!showPassword ? "password" : "text"}
+              value={password}
+              handleChange={handlePassword}
             />
+            {error.pswrd && <span className='login-message'>{error.pswrd}</span>}
           </div>
-          <div className='login-password-icon'>
-            <FaEye />
-            <FaEyeSlash />
+          <div className='login-password-icon' onClick={handlePasswordIcon}>
+            {showPassword ? <FaEye /> :<FaEyeSlash />}
           </div>          
           
         </div>
         <div className='login-forget-pswd'>Forget password?</div>
         <div className='login-button'>
-          <Button label={"Log in"} width='400px'/>
+          <Button label={"Log in"} width='400px' onClick={handleClick}/>
         </div>
         <div className='login-create-acc'>Don't have an account? Click here to Create an account.</div>
       </div>
@@ -63,7 +115,9 @@ const Login = () => {
       </div>
 
       <div className='login-google'>
-        Continue with Google
+        <div><FcGoogle /></div>
+        <div>Continue with Google</div>
+        
       </div>
 
       <div className='login-footer'>
