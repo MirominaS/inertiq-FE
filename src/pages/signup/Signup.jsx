4d840430ff2f91caa50a2e90d7b503ede4f4ff10
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-04-05 09:00:08 +0530

    added validations to signup page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -6,11 +6,20 @@ import Input from '../../components/input/Input'
 import Dropdown from '../../components/dropdown/Dropdown'
 import Button from '../../components/button/Button'
 import { MdOutlineCheckBoxOutlineBlank, MdOutlineCheckBox  } from "react-icons/md";
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 const Signup = () => {
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email,setEmail] = useState(''); 
+  const [stream, setStream] = useState('');
+  const [password,setPassword] = useState('');
+  const [confirmPassword,setConfirmPassword] = useState('');
+  const [showPassword,setShowPassword] = useState(false);
+  const [showConfPassword, setShowConfPassword] = useState(false)
+  const [error,setError] = useState({})
+  const [displayMsg,setDisplayMsg] = useState("")
+  const [showCheckbox, setShowCheckbox] = useState(false) 
 
    const streams = [
     {label:"Select",id:0},
@@ -22,13 +31,13 @@ const Signup = () => {
   ]
 
   const  handleFirstname = (fname) => {
-    setFirstname(fname.target.value)
-    console.log(firstname)
+    setFirstName(fname.target.value)
+    console.log(firstName)
   }
 
   const handleLastname = (lname) => {
-    setLastname(lname.target.value)
-    console.log(lastname)
+    setLastName(lname.target.value)
+    console.log(lastName)
   }
 
   const handleEmail = (mail) => {
@@ -36,10 +45,91 @@ const Signup = () => {
     console.log(email)
   }
 
+  const handleStream = (str) => {
+    setStream(str)
+    console.log("Stream",str)
+  }
+
+  const handlePassword = (pswd) => {
+    setPassword(pswd.target.value)
+    console.log(password)
+  } 
+
+  const handleConfirmPassword = (cpswd) => {
+    setConfirmPassword(cpswd.target.value)
+    console.log(confirmPassword)
+  }
+
+  const handlePasswordICon = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const handleConfPasswordICon = () => {
+    setShowConfPassword(!showConfPassword)
+  }
+
+  const handleCheckboxIcon = () => {
+    setShowCheckbox(!showCheckbox)
+  }
+
+   const validation = () => {
+      let error = {f_name:"",l_name:"",e_mail:"",stream:"",pswrd:"",cpswrd:"",term:""};
+      let valid = true;
+      if(!firstName.trim() && !lastName.trim() && !email.trim() && !stream.trim()){
+        error.f_name = "Firstname is required"
+        error.l_name = "Lastname is required"
+        error.e_mail = "Email is required"
+        error.stream = "Select a Stream"
+        valid = false;
+      }
+      if(!password.trim()){
+        error.pswrd = "Password is required"
+        setPassword("")
+        valid = false;
+      }else if(password.length < 8){
+        error.pswrd = "Password should contain atleast 8 char"
+        setPassword("")
+        valid = false;
+      }
+       if(!confirmPassword.trim()){
+        error.cpswrd = "Password is required"
+        setConfirmPassword("")
+        valid = false;
+      }else if(confirmPassword !== password ){
+        error.cpswrd = "Please confirm your Password"
+        setConfirmPassword("")
+        valid = false;
+      }
+      if(showCheckbox === false){
+          error.term = "Agree to terms and conditions"
+          valid = false
+      }
+      setError(error)
+      return valid;
+    }    
+
+    const handleClick = () => {
+      const validate = validation()
+      let message = ""
+      if(validate ===true){
+        message = "Signup Success!"
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setStream("");
+        setPassword("");
+        setConfirmPassword("");
+      }else{
+        message = "Please enter your details"
+      }
+      setDisplayMsg(message)
+    }
+
   return (
     <div className='signup-container'>
       <div className='signup-logo'>
         <Logo/>
+        <span className='signup-message'>{displayMsg}</span>
       </div>
       <div className='signup-input'>
         <div className='signup-input-title'>Create Account</div>
@@ -48,21 +138,25 @@ const Signup = () => {
             <Input 
               maxWidth="400px"
               type="text"
+              value={firstName}
               width='80%'
               height='35px'
               placeholder={"Firstname"}
               handleChange={handleFirstname}
             />
+            {error.f_name && <span className='signup-error-message'>{error.f_name}</span>}
           </div>
           <div className='signup-input-lastname'>
             <Input
               maxWidth="400px"
+              value={lastName}
               type="text"
               width='80%'
               height='35px'
               placeholder={"Lastname"}
               handleChange={handleLastname}
             />
+            {error.l_name && <span className='signup-error-message'>{error.l_name}</span>}
           </div>
         </div>
         <div className='signup-input-mail-stream'>
@@ -70,11 +164,13 @@ const Signup = () => {
             <Input
               maxWidth="400px"
               type="text"
+              value={email}
               width='80%'
               height='35px'
               placeholder={"Email"}
               handleChange={handleEmail}
             />
+            {error.e_mail && <span className='signup-error-message'>{error.e_mail}</span>}
           </div>
           <div className='signup-input-stream'>
             <Dropdown 
@@ -90,38 +186,57 @@ const Signup = () => {
               borderRadius="5px"
               textAlign="left"
               padding="5px"
-              color="rgb(158, 158, 158)"
-              
-            />
+              color="rgb(158, 158, 158)"     
+              dataSent={handleStream}  
+              value = {stream}    
+            />{error.stream && <span className='signup-error-message'>{error.stream}</span>}
           </div>
         </div>
         <div className='signup-input-password'>
           <div className='signup-input-pswd'>
             <Input
               maxWidth="400px"
-              type="password"
+              type={!showPassword ? "Password" : "text"}
               width='80%'
               height='35px'
               placeholder={"Password"}
+              handleChange={handlePassword}
+              value={password}
             />
+            {error.pswrd && <span className='signup-error-message'>{error.pswrd}</span>}
+            <div className='sigup-password-icon' onClick={handlePasswordICon}>
+                {showPassword ? <FaEye/> : <FaEyeSlash/>}
+            </div>
           </div>
           <div className='signup-input-conf-pswd'>
             <Input
               maxWidth="400px"
-              type="password"
+              type={!showConfPassword ? "Password" : "text"}
               width='80%'
               height='35px'
               placeholder={"Confirm Password"}
-              
+              handleChange={handleConfirmPassword} 
+              value={confirmPassword}             
             />
+            {error.cpswrd && <span className='signup-error-message'>{error.cpswrd}</span>}
+            <div className='sigup-password-icon' onClick={handleConfPasswordICon}>
+                {showConfPassword ? <FaEye/> : <FaEyeSlash/>}
+            </div>
           </div>
         </div>
         <div className='signup-input-terms'>
-          <div className='signup-input-checkbox'><MdOutlineCheckBoxOutlineBlank /><MdOutlineCheckBox /></div>
+          <div className='signup-terms'>
+            <div className='signup-input-checkbox' onClick={handleCheckboxIcon}>
+            {showCheckbox ? <MdOutlineCheckBox />  : <MdOutlineCheckBoxOutlineBlank />}
+          </div>
           <div className='signup-input-term-text'>Agree to the Terms and Conditions</div>
+
+          </div>
+          
+          {error.term && <span className='signup-error-message'>{error.term}</span>}
         </div>
         <div className='signup-input-button'>
-          <Button label={"Create Account"} width='91%'/>
+          <Button label={"Create Account"} width='91%' onClick={handleClick}/>
         </div>
       </div>
       <div className='signup-footer'>
